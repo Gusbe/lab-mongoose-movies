a8b15137f9cbf169939900d109b9997389459dc4
Gusbe <gusbertolo@gmail.com>
2019-04-29 16:44:33 +0200

    Iteration 3

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -2,12 +2,24 @@ var express = require('express');
 var router = express.Router();
 const Celebrity = require('./../models/celebrity');
 
+
+// GET '/celebrities'
+router.get('/:celebrityId', (req, res, next) => {
+  
+  const { celebrityId } = req.params;
+
+  Celebrity.findById( celebrityId )
+    .then( (celebrity) => res.render('celebrities/show', { celebrity }))
+    .catch( (err) => console.log(err));
+
+});
+
 // GET '/celebrities'
 router.get('/', (req, res, next) => {
   
   Celebrity.find({})
     .then( (allTheCelebritiesFromDB) => {
-      
+
       res.render('celebrities/index', {allTheCelebritiesFromDB})
       
     })
